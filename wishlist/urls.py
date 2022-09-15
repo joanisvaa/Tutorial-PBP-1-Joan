26f22c473b26214a6d70e987b1ddf1e14dee1961
joanisva <joan.isva@ui.ac.id>
2022-09-15 13:23:14 +0700

    Menambahkan fungsi show_json

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -8,5 +8,6 @@ app_name = 'wishlist'
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -16,6 +16,10 @@ def show_xml(request):
     data = BarangWishlist.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
 def show_json_by_id(request, id):
     data = BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
